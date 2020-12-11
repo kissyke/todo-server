83ae797eed3e3deb2f3407d5073d43737f2b9272
Edina Mocsany <Edina_Mocsany@epam.com>
2020-12-11 14:49:01 +0100

    Added static folder

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 const logFilePath = path.join(__dirname, "todoApi.log");
 
